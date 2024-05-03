363f68eafa7483a0313cecb3d3fb2636b3f7b266
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2024-05-04 01:51:44 +0200

    Upgrade setup

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
+  { ignores: ['dist', 'coverage'] },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   {
